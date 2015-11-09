1ce6c81498a1405a56e09f93e7891b7366e011a4
sergamers <sergamers@mail.ru>
2015-11-09 13:24:01 +0300

    Create tasks table

--- public/scripts/app-module.js
+++ public/scripts/app-module.js
@@ -1,4 +1,36 @@
 'use strict'
 
 
-angular.module('todos', []);
+var app = angular.module('todos', []);
+
+app.controller("TodoList", function($scope, $http){
+	$http.get('/api/v0/todos/tags').success(function(request){
+		$scope.allTags = request.items;
+	});					
+	
+	$scope.tasks = [];
+
+	var getTask = function(hash){		
+	 	var url = '/api/v0/todos/items';
+
+	 	if(hash){
+	 		url += '?hash='+hash;
+	 	}else{
+	 		hash=0;
+	 	}
+
+	 	$http.get(url).success(function(request){
+	 		$scope.tasks = $scope.tasks.concat(request.items);
+	 		if(request.finish === false){
+	 			hash++;
+	 			getTask(hash);
+	 		}
+	 	});			
+	}	
+
+	$scope.deleteTask = function(id){
+		$scope.tasks.splice(id, 1);
+	}
+
+	getTask();
+});
